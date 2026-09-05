b36e74ab99289868afad7b2756671acc62211aeb
Orssi Mp <orssimpiere5@gmail.com>
2026-09-05 23:26:12 +0100

    gestion de theme implemente

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,21 @@ import './style.css'
 // logo de l'application
 import TranslatorLogo from './assets/logo.png'
 
+// src/main.ts
+
+document.addEventListener('DOMContentLoaded', () => {
+  const themeToggle = document.querySelector<HTMLInputElement>('.theme-controller');
+
+  if (themeToggle) {
+    themeToggle.addEventListener('change', (e) => {
+      const isChecked = (e.target as HTMLInputElement).checked;
+      const html = document.documentElement;
+
+      if (isChecked) {
+        html.setAttribute('data-theme', 'dark');
+      } else {
+        html.setAttribute('data-theme', 'light');
+      }
+    });
+  }
+});
